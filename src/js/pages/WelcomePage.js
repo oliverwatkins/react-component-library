c9f41b4905f781e216d75850f911e7efd005eec5
watkinso <watkinso@hansaluftbild.de>
2018-11-28 13:58:35 +0100

    asdfasdfasdfasdf

--- src/js/pages/WelcomePage.js
+++ src/js/pages/WelcomePage.js
@@ -4,7 +4,7 @@ export default class WelcomePage extends React.Component {
   render() {
     return (
       <div className="flex-container astras_border_page">
-        <h1>Home</h1>
+        <h1>Home..</h1>
         <div>
           <ul>
 
